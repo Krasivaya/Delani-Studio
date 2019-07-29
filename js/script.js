d3a4b55ad132a62c1d757795c8870a8483ce1f0f
Krasivaya <semwagacarine@gmail.com>
2019-07-29 12:10:14 +0200

    Updated html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,92 @@
 $(document).ready(function() {
-	$('.icon1').click(function() {
-		$('.icon1_onhover').toggle();
-		$('.icon2_onhover').toggle();
-		$('.icon3_onhover').toggle();
+	$(".whatwedo_img1").click(function() {
+		$(".icon1").toggle();
+		$(".icon1_onhover").toggle();
 	});
+});
+$(document).ready(function() {
+	$(".whatwedo_p1").click(function() {
+		$(".icon1").toggle();
+		$(".icon1_onhover").toggle();
+	});
+});
+$(document).ready(function() {
+	$(".whatwedo_img2").click(function() {
+		$(".icon2").toggle();
+		$(".icon2_onhover").toggle();
+	});
+});
+$(document).ready(function() {
+	$(".whatwedo_p2").click(function() {
+		$(".icon2").toggle();
+		$(".icon2_onhover").toggle();
+	});
+});
+$(document).ready(function() {
+	$(".whatwedo_img3").click(function() {
+		$(".icon3").toggle();
+		$(".icon3_onhover").toggle();
+	});
+});
+$(document).ready(function() {
+	$(".whatwedo_p3").click(function() {
+		$(".icon3").toggle();
+		$(".icon3_onhover").toggle();
+	});
+});
+$('.hover1').mouseover(function() {
+  $('.text1').css("visibility","visible");
+});
+
+$('.hover1').mouseout(function() {
+  $('.text1').css("visibility","hidden");
+});
+$('.hover2').mouseover(function() {
+  $('.text2').css("visibility","visible");
+});
+
+$('.hover2').mouseout(function() {
+  $('.text2').css("visibility","hidden");
+});
+$('.hover3').mouseover(function() {
+  $('.text3').css("visibility","visible");
+});
+
+$('.hover3').mouseout(function() {
+  $('.text3').css("visibility","hidden");
+});
+$('.hover4').mouseover(function() {
+  $('.text4').css("visibility","visible");
+});
+
+$('.hover4').mouseout(function() {
+  $('.text4').css("visibility","hidden");
+});
+$('.hover5').mouseover(function() {
+  $('.text5').css("visibility","visible");
+});
+
+$('.hover5').mouseout(function() {
+  $('.text5').css("visibility","hidden");
+});
+$('.hover6').mouseover(function() {
+  $('.text6').css("visibility","visible");
+});
+
+$('.hover6').mouseout(function() {
+  $('.text6').css("visibility","hidden");
+});
+$('.hover7').mouseover(function() {
+  $('.text7').css("visibility","visible");
+});
+
+$('.hover7').mouseout(function() {
+  $('.text7').css("visibility","hidden");
+});
+$('.hover8').mouseover(function() {
+  $('.text8').css("visibility","visible");
+});
+
+$('.hover8').mouseout(function() {
+  $('.text8').css("visibility","hidden");
 });
